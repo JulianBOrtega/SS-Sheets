60a7b9e9ae3b14611214ff8e38856f717f7e2166
Julián <j.gakusei11@gmail.com>
2024-11-14 20:39:42 -0300

    debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useRef } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+const url = useRef(import.meta.env.VITE_API_URL);
+useEffect(() => {
+  
+  console.log('BACKEND URL', url.current);
+}, []);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
