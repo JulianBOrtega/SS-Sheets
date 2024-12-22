930a756b9fea5e99448b894df2533b4ff3abe42d
Julián <j.gakusei11@gmail.com>
2024-12-22 19:59:59 -0300

    modifiers changes

--- src/utility/sheetCalc.ts
+++ src/utility/sheetCalc.ts
@@ -2,11 +2,11 @@ import { ICharacter } from "../interfaces/character";
 import { getClass } from "./hardcoded";
 
 export const getStatModifier = (rawStat: number) => {
-    return rawStat >= 22 ? 5
-        : rawStat >= 20 ? 4
-        : rawStat >= 18 ? 3
-        : rawStat >= 16 ? 2
-        : rawStat >= 13 ? 1
+    return rawStat >= 20 ? 5
+        : rawStat >= 18 ? 4
+        : rawStat >= 16 ? 3
+        : rawStat >= 14 ? 2
+        : rawStat >= 12 ? 1
         : rawStat >= 10 ? 0
         : -1;
 }
